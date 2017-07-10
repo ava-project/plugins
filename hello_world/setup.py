bbdae7d14c33eecbfb9be0177366dd22faad221f
Tommy <thomas.milox@epitech.eu>
2017-07-10 18:20:56 +0200

    update hello_world

--- hello_world/setup.py
+++ hello_world/setup.py
@@ -1,6 +1,8 @@
+import os
 from distutils.core import setup, Extension
 
-module = Extension('hello_world', sources=['core/plugins_manager/plugins/hello_world/hello_world.cpp'])
+path = os.path.join(os.path.expanduser('~'), '.ava', 'plugins', 'hello_world', 'hello_world.cpp')
+module = Extension('hello_world', sources=[path])
 setup(name = 'packagename',
     version='1.0',
     description = 'a test package',
