6eb30fc37c7d975bb40a308bbf363b896d4c589f
Tommy <thomas.milox@epitech.eu>
2017-08-20 20:47:04 +0100

    added plugin 'test' for testing interaction/crash/oneshot command

---
+++ test/test.py
@@ -0,0 +1,25 @@
+from avasdk.plugins.python_model import PythonModel
+
+class test(PythonModel):
+    """
+        test plugin
+    """
+
+    def __init__(self, name="test"):
+        super(test, self).__init__(name)
+        self.set_commands_list({**PythonModel._commands, **{\
+        "oneshot" : self.oneshot, \
+        "interaction": self.interaction, \
+        "crash": self.crash, \
+        }})
+
+    def crash(self, command):
+        """
+        """
+        return toto
+
+    def oneshot(self, command):
+        print('### ONESHOT ###')
+
+    def interaction(self, command):
+        print(str(self.wait_for_user(self, 'test', 'say anything')).upper())
